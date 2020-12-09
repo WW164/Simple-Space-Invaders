07a717f9980659c465ded71d82ef3bb895f8df45
parinaz mirbagheri <parinazmirbagheri@gmail.com>
2020-12-09 08:20:19 -0800

    Implement enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : Shape
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+    private void MoveEnemy()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime, Space.World);
+
+        float bottom = transform.position.y - halfHeight;
+
+        if (bottom <= -screenBounds.y)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public EnemyMovement enemyPrefab;
+    public Vector3 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = GetScreenBounds();
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnEnemies ()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            WaitForSeconds wait = new WaitForSeconds(2);
+
+            float horizontalPosition = Random.Range(-screenBounds.x, screenBounds.x);
+            Vector2 spawnPosition = new Vector2(horizontalPosition, screenBounds.y);
+
+            EnemyMovement enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            yield return wait;
+        }
+    }
+
+    private Vector3 GetScreenBounds()
+    {
+        Camera mainCamera = Camera.main;
+        Vector3 screenVector = new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z);
+
+        return mainCamera.ScreenToWorldPoint(screenVector);
+    }
+}
